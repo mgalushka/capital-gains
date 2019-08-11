27499542d4c35f7a7cf238ec1522dc18340f54e6
BuildTools <unconfigured@null.spigotmc.org>
2019-08-11 22:21:18 +0100

    working build

--- src/capitalgains.js
+++ src/capitalgains.js
@@ -4,5 +4,12 @@ import type currency from "./currency"
 
 type CapitalGainType = {
   amount: number,
-
+  currency: currency,
 };
+
+const gains = () => {
+  return {
+    amount: 100,
+    currency: 'GBP',
+  }
+}
