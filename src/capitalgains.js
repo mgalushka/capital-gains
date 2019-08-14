63eaf3e32bb1eb5fe10e9dd6c1005984a1a05092
BuildTools <unconfigured@null.spigotmc.org>
2019-08-14 21:21:21 +0100

    simple API t ocompute current balance

--- src/capitalgains.js
+++ src/capitalgains.js
@@ -11,9 +11,30 @@ type Transaction = {
 
 class Gains {
   transactions: Array<TransactionType> = new Array();
+
+  // accumulate transaction for later computation
   add(transaction: TransactionType): Gains {
     this.transactions.push(transaction);
     return this;
+  };
+
+  // computing total balance, if transaction is BUY we add, SELL - subtract
+  balance(): number {
+    return this.transactions.reduce(
+      (accumulator, trx) => {
+        if (trx.type === "BUY") {
+          return accumulator + trx.amount;
+        } else {
+          return accumulator - trx.amount;
+        }
+      },
+      0,
+    );
+  };
+
+  // returns number of transactions
+  count(): number {
+    return this.transactions.length;
   }
 };
 
--- test/capitalgains.test.js
+++ test/capitalgains.test.js
@@ -15,4 +15,24 @@ test('basic accumulation', () => {
     type: "BUY",
   });
   expect(g.transactions.length).toBe(2);
+  expect(g.count()).toBe(2);
+});
+
+test('basic balance', () => {
+  const g = gains();
+  g.add({
+    amount: 10,
+    currency: "USD",
+    type: "SELL",
+  });
+  expect(g.transactions.length).toBe(1);
+
+  g.add({
+    amount: 15,
+    currency: "USD",
+    type: "BUY",
+  });
+
+  // js array reduce example: https://jsfiddle.net/pym47ung/1/
+  expect(g.balance()).toBe(5);
 });
